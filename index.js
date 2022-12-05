07f8c98aeacea6d4e20448dea37592822a22a148
Tejal94 <tejalshigwan@gmail.com>
2022-12-05 13:53:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function background2(){
+
+    let element = document.getElementsByClassName("main");
+    element.style.backgroundImage = "url('./images/back2.jpg')";
+
+}
+
+
+// button2.addEventListener('onclick',background2);
+
+
